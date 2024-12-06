b024083d871d37fc822919c529d606d1bda73fde
Daniel Portugal <dpcosta@gmail.com>
2024-12-06 08:16:19 -0300

    Nova forma de registrar clientes

---
+++ ContainRs.WebApp/Controllers/ApiRegistroController.cs
@@ -0,0 +1,37 @@
+﻿using ContainRs.WebApp.Data;
+using ContainRs.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContainRs.WebApp.Controllers;
+
+[ApiController]
+[Route("api/registros")]
+public class ApiRegistroController : ControllerBase
+{
+    private readonly AppDbContext context;
+
+    public ApiRegistroController(AppDbContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(RegistroViewModel request)
+    {
+        var cliente = new Cliente(request.Nome, new Email(request.Email), request.CPF)
+        {
+            Celular = request.Celular,
+            CEP = request.CEP,
+            Rua = request.Rua,
+            Numero = request.Numero,
+            Complemento = request.Complemento,
+            Bairro = request.Bairro,
+            Municipio = request.Municipio,
+            Estado = request.Estado
+        };
+        context.Clientes.Add(cliente);
+        await context.SaveChangesAsync();
+
+        return Ok();
+    }
+}

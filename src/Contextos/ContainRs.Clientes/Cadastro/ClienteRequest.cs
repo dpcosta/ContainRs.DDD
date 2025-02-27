02d3fd6b1a9e91e79208f86ad6224437490c8fa6
Daniel Portugal <dpcosta@gmail.com>
2025-02-27 08:10:19 -0300

    Separando o contexto de clientes

--- src/Contextos/ContainRs.Clientes/Cadastro/ClienteRequest.cs
+++ src/Contextos/ContainRs.Clientes/Cadastro/ClienteRequest.cs
@@ -1,7 +1,6 @@
-﻿using ContainRs.Domain.Models;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace ContainRs.Api.Clientes;
+namespace ContainRs.Clientes.Cadastro;
 
 public class EnderecoRequest
 {

6d37081ea968385d3c4e98f2d741eb9d813e6785
Daniel Portugal <dpcosta@gmail.com>
2025-02-27 16:30:20 -0300

    Ao executar a API, percebi que o modelo estava diferente da estrutura de dados. Incluí essa migration para isso.

--- src/Apps/ContainRs.Api/Data/Configurations/SolicitacaoConfigurations.cs
+++ src/Apps/ContainRs.Api/Data/Configurations/SolicitacaoConfigurations.cs
@@ -13,5 +13,23 @@ public class SolicitacaoConfigurations : IEntityTypeConfiguration<PedidoLocacao>
                 .HasColumnName("Status")
                 .HasConversion<string>();
         });
+
+        builder.OwnsOne(s => s.Localizacao, loc =>
+        {
+            loc.Property(l => l.CEP)
+                .HasColumnName("CEP")
+                .IsRequired();
+
+            loc.Property(l => l.Referencias)
+                .HasColumnName("Referencias");
+
+            loc.Property(l => l.Latitude)
+                .HasColumnName("Latitude")
+                .HasColumnType("decimal(18,6)");
+
+            loc.Property(l => l.Longitude)
+                .HasColumnName("Longitude")
+                .HasColumnType("decimal(18,6)");
+        });
     }
 }
---
+++ src/Apps/ContainRs.Api/Data/Migrations/20250227192503_LocalizacaoPedido.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContainRs.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class LocalizacaoPedido : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Solicitacoes_Endereco_EnderecoId",
+                table: "Solicitacoes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Solicitacoes_EnderecoId",
+                table: "Solicitacoes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CEP",
+                table: "Solicitacoes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Latitude",
+                table: "Solicitacoes",
+                type: "decimal(18,6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Longitude",
+                table: "Solicitacoes",
+                type: "decimal(18,6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Referencias",
+                table: "Solicitacoes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Conteineres",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LocacaoId",
+                table: "Conteineres",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CEP",
+                table: "Solicitacoes");
+
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Solicitacoes");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Solicitacoes");
+
+            migrationBuilder.DropColumn(
+                name: "Referencias",
+                table: "Solicitacoes");
+
+            migrationBuilder.DropColumn(
+                name: "LocacaoId",
+                table: "Conteineres");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Conteineres",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Solicitacoes_EnderecoId",
+                table: "Solicitacoes",
+                column: "EnderecoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Solicitacoes_Endereco_EnderecoId",
+                table: "Solicitacoes",
+                column: "EnderecoId",
+                principalTable: "Endereco",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
